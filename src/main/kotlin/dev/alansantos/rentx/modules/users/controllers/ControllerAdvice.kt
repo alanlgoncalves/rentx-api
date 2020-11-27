01e17f23d1fbc6ca6d9802c8cdbbb98807d0ca84
alanlgoncalves <alan.lgoncalves@outlook.com>
2020-11-27 20:35:52 -0300

    Add unprocessable entity on controller advice and code refactor

--- src/main/kotlin/dev/alansantos/rentx/modules/users/controllers/ControllerAdvice.kt
+++ src/main/kotlin/dev/alansantos/rentx/modules/users/controllers/ControllerAdvice.kt
@@ -1,6 +1,7 @@
 package dev.alansantos.rentx.modules.users.controllers
 
 import dev.alansantos.rentx.modules.users.exceptions.AuthenticationException
+import dev.alansantos.rentx.modules.users.exceptions.UserAlreadyExistsException
 import dev.alansantos.rentx.shared.dto.AppError
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.ControllerAdvice
@@ -14,7 +15,14 @@ class ControllerAdvice {
     @ResponseBody
     @ExceptionHandler(value = [AuthenticationException::class])
     @ResponseStatus(HttpStatus.UNAUTHORIZED)
-    fun handleAuthenticationException(ex: Throwable): AppError {
+    fun handleUnauthorized(ex: Throwable): AppError {
+        return AppError(ex.message)
+    }
+
+    @ResponseBody
+    @ExceptionHandler(value = [UserAlreadyExistsException::class])
+    @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
+    fun handleUnprocessableEntity(ex: Throwable): AppError {
         return AppError(ex.message)
     }
 
